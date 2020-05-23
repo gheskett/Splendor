c1a836c66bac0fc174074130e7bec7e9d11f885c
Cameron Heskett <cameron517@gmail.com>
2020-05-23 02:29:38 -0700

    Added Basic Main Menu
    - Added Menu screen with game title and New and Join game buttons
    - Buttons are highligted on hover to show selection
    - Added assets for tile and menu buttons

--- phaser/src/index.js
+++ phaser/src/index.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import newGame from "./assets/new_game.svg";
+import joinGame from "./assets/join_game.svg";
+import titleLogo from "./assets/title.svg"
+import background from "./assets/pattern-background-frost-texture.jpg"
 
 const config = {
   type: Phaser.AUTO,
@@ -15,18 +18,37 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.svg("newGame", newGame);
+  this.load.svg("joinGame", joinGame);
+  this.load.svg("title", titleLogo);
+  this.load.image("bg", background);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  const SELECTED = 1
+  const NOT_SELECTED = 0.7
+
+  const bg = this.add.image(0, 0, "bg").setOrigin(0).setScale(1.5);
+  const title = this.add.image(400, 100, "title");
+
+  const newGame = this.add.sprite(400, 300, "newGame").setInteractive().setAlpha(NOT_SELECTED);
+  const joinGame = this.add.image(400, 400, "joinGame").setInteractive().setAlpha(NOT_SELECTED);
+
+  newGame.on('pointerover',function(pointer){
+  newGame.setAlpha(SELECTED);
   });
+
+  newGame.on('pointerout',function(pointer){
+    newGame.setAlpha(NOT_SELECTED);
+  });
+
+  joinGame.on('pointerover',function(pointer){
+    joinGame.setAlpha(SELECTED);
+  });
+  
+  joinGame.on('pointerout',function(pointer){
+    joinGame.setAlpha(NOT_SELECTED);
+  });
+
 }
