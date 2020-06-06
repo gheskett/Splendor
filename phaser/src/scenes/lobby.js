275a94e72ac0ae93d78ecc20fec22eac050b08f6
Zachary Heskett <zheskett@gmail.com>
2020-06-05 22:06:22 -0500

    Added ability to change username

--- phaser/src/scenes/lobby.js
+++ phaser/src/scenes/lobby.js
@@ -1,6 +1,9 @@
 import Phaser from "phaser"
 import sessionIDText from "../assets/html/session_id_text.html"
 import lobbyBox from "../assets/html/lobby_box.html"
+import changeUsernameForm from "../assets/html/change_username_form.html"
+
+import blackRectangle from "../assets/images/black_rectangle.png"
 
 export default class lobby extends Phaser.Scene {
     constructor(config) {
@@ -19,10 +22,15 @@ export default class lobby extends Phaser.Scene {
     preload() {
         this.load.html("lobbyIDText", sessionIDText);
         this.load.html("lobbyBox", lobbyBox);
+        this.load.html("changeUsernameForm", changeUsernameForm);
+        this.load.image("dimmingObject", blackRectangle);
     }
 
     create() {
         var thisLobby = this;
+
+        //#region Game Variables
+        const DIM = 0.75
 
         var numOfPlayers = -1;
         const lobbyColors = ["red", "rgb(0, 119, 255)", "rgb(200, 0, 200)", "rgb(0, 185, 0)"]
@@ -30,6 +38,11 @@ export default class lobby extends Phaser.Scene {
         var lobbyIDText = this.add.dom(gameWidth / 2, 100).createFromCache("lobbyIDText").setOrigin(0.5, 1);
         var lobbyBoxes = [];
         lobbyIDText.getChildByID("idValue").innerHTML = this.lobbyID;
+
+        const dimmingObject = this.add.image(0, 0, "dimmingObject").setOrigin(0).setAlpha(DIM).setVisible(false).setDepth(1);
+        var changeUsernameForm = this.add.dom(gameWidth / 2, gameHeight / 2 - 80).createFromCache("changeUsernameForm").setVisible(false).setDepth(2);
+
+        //#endregion Game Variables
 
         fetch(this.fullAddr + "/api/is_game_started?" + new URLSearchParams({
             session_id: thisLobby.lobbyID,
@@ -51,30 +64,99 @@ export default class lobby extends Phaser.Scene {
 
         //#endregion Server Listeners
 
-        function updateLobby(data) {
-            console.log(data);
-            
-            if (numOfPlayers !== Object.keys(data.players).length) {
-                lobbyBoxes = [];
-                for (var i = 0; i < Object.keys(data.players).length; i++) {
-                    lobbyBoxes[i] = thisLobby.add.dom(((i % 2) * (gameWidth / 2)), (i >= 2 ? 300 : 0) + 200).createFromCache("lobbyBox").setOrigin(0);
+        //#region Form Behavior
 
-                    if (data.host_id === i) {
-                        lobbyBoxes[i].getChildByID("host").style.display = "inline-block";
-                    }
+        changeUsernameForm.addListener("click");
+        changeUsernameForm.on("click", function (event) {
 
-                    if (thisLobby.playerID === i) {
-                        lobbyBoxes[i].getChildByID("pencil").style.display = "inline-block";
-                        lobbyBoxes[i].getChildByID("userContainer").style.border = "10px solid gold";
-                    }
+            console.log(event);
+            var newName = this.getChildByName("usernameField").value;
 
-                    lobbyBoxes[i].getChildByID("usernameValue").innerHTML = data.players[Object.keys(data.players)[i]].username;
-                    lobbyBoxes[i].getChildByID("playerValue").innerHTML = i + 1;
-                    lobbyBoxes[i].getChildByID("userContainer").style.background = lobbyColors[i];
+            if (event.target.name === "change") {
+
+                var args = {
+                    player_id: thisLobby.playerID,
+                    username: newName,
+                    session_id: thisLobby.lobbyID
                 }
-                numOfPlayers = Object.keys(data.players).length;
+                fetch(thisLobby.fullAddr + "/api/change_username/", {
+                    method: "POST",
+                    headers: thisLobby.headers,
+                    body: JSON.stringify(args)
+                }).then(handleErrors)
+                .then(result => {
+                    console.log(result);
+                    if (result === "OK") {
+                        console.log("Username Changed to " + newName);
+                    } else {
+                        console.warn("Username Chnage Failed", result);
+                    }
+                }).catch(error => {
+                    console.error("Error: ", error);
+                });
+                
+                this.setVisible(false);
+                //toggleLobbyElements(true);
+            
             }
 
+            //Enable play buttons and remove username form on cancel
+            if (event.target.name === "cancel") {
+
+                this.setVisible(false);
+                //toggleLobbyElements(true);
+
+            }
+        });
+
+        //#endregion Form Behavior
+
+        /**
+         * @param {boolean} enable
+         * Enables/Disables Lobby Elements
+         */
+        function toggleLobbyElements(enable) {
+            if (enable) {
+                for (var i = 0; i < lobbyBoxes.length; i++) {
+                    lobbyBoxes[i].setInteractive();
+                }
+                dimmingObject.setVisible(false);
+            } 
+            else {
+                for (var i = 0; i < lobbyBoxes.length; i++) {
+                    lobbyBoxes[i].disableInteractive();
+                }
+                dimmingObject.setVisible(true);
+            }
+        }
+
+        function updateLobby(data) {
+            console.log(data);
+            lobbyBoxes = [];
+             for (var i = 0; i < Object.keys(data.players).length; i++) {
+                lobbyBoxes[i] = thisLobby.add.dom(((i % 2) * (gameWidth / 2)), (i >= 2 ? 300 : 0) + 200).createFromCache("lobbyBox").setOrigin(0).setDepth(0);
+
+                if (data.host_id === i) {
+                    lobbyBoxes[i].getChildByID("host").style.display = "inline-block";
+                }
+
+                if (thisLobby.playerID === i) {
+                    lobbyBoxes[i].getChildByID("pencil").style.display = "inline-block";
+                    lobbyBoxes[i].getChildByID("userContainer").style.border = "10px solid gold";
+                    lobbyBoxes[i].addListener("click");
+                    lobbyBoxes[i].on("click", function(event) {
+                        if (event.target.id = "pencil") {
+                            changeUsernameForm.setVisible(true);
+                            //toggleLobbyElements(false);
+                        }
+                    });
+                }
+
+                lobbyBoxes[i].getChildByID("usernameValue").innerHTML = data.players[Object.keys(data.players)[i]].username;
+                lobbyBoxes[i].getChildByID("playerValue").innerHTML = i + 1;
+                lobbyBoxes[i].getChildByID("userContainer").style.background = lobbyColors[i];
+            }
+            numOfPlayers = Object.keys(data.players).length;
         }
 
         function handleErrors(response) {
